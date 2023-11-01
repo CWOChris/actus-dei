434636b856d97ad474b0126e7c2a4a8f58989daf
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-11-01 15:32:05 -0500

    navbar errors

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function initAutoComplete() {
+    var input = document.getElementById('city');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var place = autocomplete.getPlace();
+    });
+};
