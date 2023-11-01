1bab4ad38a685e9afa69d94b0ce4fb9fcb3699b6
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-11-01 17:53:42 -0500

    played around with the bootstrap layout on the DOM some more

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// // BEGIN AUTOCOMPLETE
+document.addEventListener('DOMContentLoaded', function () {
+    initAutoComplete();
+});
+
+
 function initAutoComplete() {
     var input = document.getElementById('city');
     var autocomplete = new google.maps.places.Autocomplete(input);
@@ -6,8 +12,36 @@ function initAutoComplete() {
         var place = autocomplete.getPlace();
     });
 };
+// // END AUTOCOMPLETE
 
-document.addEventListener('DOMContentLoaded', function () {
-    initAutoComplete();
-});
 
+
+
+
+// //  MIGHT BE ABLE TO USE THE CODE BELOW FOR THE WEATHER IMAGE IN THE CARDS
+
+
+// const myAPIKey = '442fc2578f087c163d126cb7d628bf5d';
+// const city = "Minneapolis, MN";
+// const wXAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}`;
+
+// fetch(wXAPIURL)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error("OpenWeather API request failed");
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         const weatherIcon = data.weather[0].icon;
+//         const weatherIconURL = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+//         const weatherImage = document.createElement('img');
+//         weatherImage.src = weatherIconURL;
+
+//         document.getElementById('wxImg').appendChild(weatherImage);
+//     })
+//     .catch(error => {
+//         console.error('Error: ', error);
+//     });
+
+// //  END OF THE CODE FOR THE WEATHER IMAGE IN THE CARDS
