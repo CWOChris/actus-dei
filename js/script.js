dca3a5b5d85ca9cc1419965050b0d13aedf5ab6b
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-11-01 23:32:36 -0500

    getting the API stuff to work

--- js/script.js
+++ js/script.js
@@ -22,27 +22,70 @@ function initAutoComplete() {
 
 // //  MIGHT BE ABLE TO USE THE CODE BELOW FOR THE WEATHER IMAGE IN THE CARDS
 
-// const myAPIKey = '442fc2578f087c163d126cb7d628bf5d';
-// const city = "Minneapolis, MN";
-// const wXAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}`;
+const myAPIKey = '442fc2578f087c163d126cb7d628bf5d';
+document.getElementById('citySearch').addEventListener('click', () => {
+    const city = document.getElementById('city').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=celsius&appid=${myAPIKey}`)
+        .then(response => response.json())
+        .then(data => {
+            displayWeatherForecast(data);
+        })
+        .catch(err => {
+            console.log('Error: ', error);
+        });
+});
 
-// fetch(wXAPIURL)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error("OpenWeather API request failed");
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         const weatherIcon = data.weather[0].icon;
-//         const weatherIconURL = `http://openweathermap.org/img/w/${weatherIcon}.png`;
-//         const weatherImage = document.createElement('img');
-//         weatherImage.src = weatherIconURL;
+function displayWeatherForecast(data) {
+    const forecastContainer = document.getElementById('#');
+    forecastContainer.innerHTML = '';
 
-//         document.getElementById('wxImg').appendChild(weatherImage);
-//     })
-//     .catch(error => {
-//         console.error('Error: ', error);
-//     });
+    data.list.forEach(item => {
+        const date = new Date(item.dt * 1000);
+        const temperature = item.main.temp;
+        const description = item.weather[0].description;
+        const icon = item.weather[0].icon;
+        const humidity = item.main.humidity;
+        const wind = item.wind.speed;
+        const sunset = new Date(item.sys.sunset * 1000);
+        const moonPhase = item.moon_phase;
+        const pressure = item.main.pressure;
+        const cityDisplay = data.city.name;
+
+        // This is where the DOM will be manipulated
+        const forecastItem = document.createElement('div');
+        forecastItem.classList.add('forecast-item');
+        forecastItem.innerHTML = `
+
+        `;
+        // End of where the DOM will be manipulated
+    });
+}
+
+
+
+
+
+
+
 
 // //  END OF THE CODE FOR THE WEATHER IMAGE IN THE CARDS
+
+// //  MIGHT BE HELPFUL LATER ON FOR THE DISPLAY OF THE WEATHER IN THE CARDS
+
+        // .then(data => {
+        //     console.log(data);
+        //     document.getElementById('cityName').innerHTML = data.name;
+        //     document.getElementById('temp').innerHTML = data.main.temp;
+        //     document.getElementById('humidity').innerHTML = data.main.humidity;
+        //     document.getElementById('wind').innerHTML = data.wind.speed;
+        //     document.getElementById('weather').innerHTML = data.weather[0].main;
+        //     document.getElementById('weatherDescription').innerHTML = data.weather[0].description;
+        //     document.getElementById('weatherIcon').src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        // })
+        // .catch(err => console.log(err));
+
+// //  END OF THE CODE FOR THE DISPLAY OF THE WEATHER IN THE CARDS
+
+// //  WX IMAGES
+        // const icon = item.weather[0].icon;
+// //  END OF WX IMAGES
